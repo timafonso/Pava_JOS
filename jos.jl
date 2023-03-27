356d4897b99c2be3dfc8a6c7e42f1558a960d863
JoaoSilva2 <jdhsilva2000@gmail.com>
2023-03-27 17:01:17 +0100

    Refactored code. There is only instances now
    Removed struct Class
    slots are all in arrays

--- jos.jl
+++ jos.jl
@@ -1,65 +1,78 @@
-struct Class
-    name::Symbol
-    direct_superclasses::Vector{Class}
-    direct_slots::Vector{Symbol}
-    # cpl::Vector{Class}
-end
-
-struct Instance
-    metaclass::Class
-    values::Vector
+mutable struct Instance
+    metaclass::Instance
+    slots::Vector{}  # In classes index 1 is superclasses and index 2 is direct slots
+    Instance() = new()
     Instance(metaclass) = new(metaclass, [])
 end
 
-## Instances
+####################################################################
+#                         BASE CLASSES                             #
+####################################################################
+
+#------------------------- class Class -----------------------------
+Class = Instance()
+Class.metaclass = Class
+Class.slots = []
+
+Object = Instance()
+Object.metaclass = Class
+
+
+push!(Class.slots, [Object])
+push!(Class.slots, [:direct_superclasses, :direct_slots])
+####################################################################
+
+####################################################################
+#                            UTILS                                 #
+####################################################################
+function get_direct_slots(class::Instance)
+    idx = findfirst(==(:direct_slots), class.metaclass.slots[2])
+    class.slots[idx]
+end
+
+get_all_slots(instance::Instance) = println("Not Implemented Exception")
+
+get_field_index(instance::Instance, slot_name::Symbol) =
+    findfirst(==(slot_name), get_direct_slots(instance.metaclass))
+
+function getproperty(instance::Instance, slot_name::Symbol)
+    idx = get_field_index(instance, slot_name)
+    instance.slots[idx]
+end
+
+function setproperty!(instance::Instance, slot_name::Symbol, value)
+    idx = get_field_index(instance, slot_name)
+    instance.slots[idx] = value
+end
+####################################################################
+
+
+####################################################################
+#                             METHODS                              #
+####################################################################
+allocate_instance(class::Instance) = Instance(class)
+
+function initialize(instance::Instance, initargs)
+    for slot_name in get_direct_slots(instance.metaclass)
+        value = get(initargs, slot_name, missing)
+        push!(instance.slots, value)
+    end
+end
+
 new(class; initargs...) =
     let instance = allocate_instance(class)
         initialize(instance, initargs)
         instance
     end
 
-allocate_instance(class::Class) = Instance(class)
+####################################################################
 
-function initialize(instance::Instance, initargs)
-    # TODO handle UndefKeywordError with our own exception
-    for slot_name in instance.metaclass.direct_slots
-        value = get(initargs, slot_name, missing)
-        push!(instance.values, value)
-    end
-end
+ComplexNumber = new(Class, direct_superclasses=[Object], direct_slots=[:real, :img])
+ist = new(ComplexNumber, img=1, real=3)
+dump(ist)
 
-
-## Bootstraping
-
-# Top
-
-# Object
-Object = Class(:Object, [], [])
-
-# Class
-# Class = Class(:Class, [Object], [])
-
-## General utilites (probably not going into macros)
-get_field_index(instance::Instance, slot_name::Symbol) =
-    findfirst(==(slot_name), instance.metaclass.direct_slots)
-
-## Accessors (going into macros)
-function getproperty(instance::Instance, slot_name::Symbol)
-    idx = get_field_index(instance, slot_name)
-    instance.values[idx]
-end
-
-ComplexNumber = Class(:ComplexNumber, [Object], [:real, :img])
-ist = new(ComplexNumber, img=2)
-
+getproperty(ist, :img)
 getproperty(ist, :real)
 
-
-## =======
-## TESTING
-## =======
-
-
-
-dump(ComplexNumber)
-
+setproperty!(ist, :real, 5634753645763485863)
+getproperty(ist, :real)
