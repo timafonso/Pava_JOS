1898df5d325cae5e154d7724bea5379ac43bddb5
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-03-27 17:31:20 +0100

    feat(classof + accessors): get with dot notation

--- jos.jl
+++ jos.jl
@@ -26,26 +26,32 @@ push!(Class.slots, [:direct_superclasses, :direct_slots])
 #                            UTILS                                 #
 ####################################################################
 function get_direct_slots(class::Instance)
-    idx = findfirst(==(:direct_slots), class.metaclass.slots[2])
-    class.slots[idx]
+    metaclass = getfield(class, :metaclass)
+    slots = getfield(metaclass, :slots)
+    dump(slots)
+    idx = findfirst(==(:direct_slots), slots[2])
+    getfield(class, :slots)[idx]
 end
 
 get_all_slots(instance::Instance) = println("Not Implemented Exception")
 
 get_field_index(instance::Instance, slot_name::Symbol) =
-    findfirst(==(slot_name), get_direct_slots(instance.metaclass))
+    findfirst(==(slot_name), get_direct_slots(getfield(instance, :metaclass)))
+####################################################################
 
-function getproperty(instance::Instance, slot_name::Symbol)
+function class_of(instance::Instance)
+    getfield(instance, :metaclass)
+end
+
+function Base.getproperty(instance::Instance, slot_name::Symbol)
     idx = get_field_index(instance, slot_name)
-    instance.slots[idx]
+    getfield(instance, :slots)[idx]
 end
 
 function setproperty!(instance::Instance, slot_name::Symbol, value)
     idx = get_field_index(instance, slot_name)
-    instance.slots[idx] = value
+    getfield(instance, :slots)[idx] = value
 end
-####################################################################
-
 
 ####################################################################
 #                             METHODS                              #
@@ -53,9 +59,10 @@ end
 allocate_instance(class::Instance) = Instance(class)
 
 function initialize(instance::Instance, initargs)
-    for slot_name in get_direct_slots(instance.metaclass)
+    dump(getfield(instance, :metaclass))
+    for slot_name in get_direct_slots(getfield(instance, :metaclass))
         value = get(initargs, slot_name, missing)
-        push!(instance.slots, value)
+        push!(getfield(instance, :slots), value)
     end
 end
 
@@ -71,8 +78,11 @@ ComplexNumber = new(Class, direct_superclasses=[Object], direct_slots=[:real, :i
 ist = new(ComplexNumber, img=1, real=3)
 dump(ist)
 
+class_of(Class) == Class
+
 getproperty(ist, :img)
 getproperty(ist, :real)
+ist.real
 
 setproperty!(ist, :real, 5634753645763485863)
 getproperty(ist, :real)
