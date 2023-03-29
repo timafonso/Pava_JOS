0a38a691a041702b37f281f7773bae16619c6847
DuBento <duarte.bento@tecnico.ulisboa.pt>
2023-03-29 16:22:57 +0100

    Feat(cpl): implemented class precedence list.
    Co-authored-by: Rodrigo Santos <rodrigo-gsantos@users.noreply.github.com>
    Co-authored-by: JoaoSilva2 <jdhsilva2000@gmail.com>
    Co-authored-by: Martim Afonso <timafonso@users.noreply.github.com>

--- jos.jl
+++ jos.jl
@@ -1,30 +1,40 @@
 #=============== CONSTS =================#
-const CLASS_SLOTS = [:name, :direct_superclasses, :direct_slots]
+const CLASS_NAME = :name
+const DIRECT_SUPERCLASSES = :direct_superclasses
+const DIRECT_SLOTS = :direct_slots
+const CLASS_CPL = :cpl
+const CLASS_SLOTS = [CLASS_NAME, DIRECT_SUPERCLASSES, DIRECT_SLOTS, CLASS_CPL]
 
 
 #=========== Instance Struct =============#
 mutable struct Instance
     class::Instance
-    slots::Vector{}  # In classes index 1 is superclasses and index 2 is direct slots
-    Instance() = new()
+    slots::Vector{}  # In classes index 1 is superclasses and index 2 is direct slots, ...
+    Instance() = (x = new(); x.class = x; x.slots = []; x)
     Instance(class) = new(class, [])
 end
 
 ####################################################################
-#                         BASE CLASSES                             #
+#                         BASE CLASSES (BOOTSTRAPPING)             #
 ####################################################################
 
 #---------------------- Class & Object -----------------------------
 Class = Instance()
-Class.class = Class
-Class.slots = []
+# Class.class = Class
+# Class.slots = []
 
-Object = Instance()
-Object.class = Class
+Object = Instance(Class)
 
-push!(Class.slots, :Class)
-push!(Class.slots, [Object])
-push!(Class.slots, CLASS_SLOTS)
+push!(Class.slots, :Class)              # name
+push!(Class.slots, [Object])            # superclasses
+push!(Class.slots, CLASS_SLOTS)         # direct slots
+push!(Class.slots, [Class, Object])     # cpl
+
+push!(Object.slots, :Object)            # name
+push!(Object.slots, [])                 # superclasses
+push!(Object.slots, [])                 # direct slots
+push!(Class.slots, [Object])     # cpl
+
 
 ####################################################################
 
@@ -32,15 +42,32 @@ push!(Class.slots, CLASS_SLOTS)
 #                            UTILS                                 #
 ####################################################################
 function get_direct_slots(class::Instance)
-    idx = findfirst(==(:direct_slots), CLASS_SLOTS)
+    idx = findfirst(==(DIRECT_SLOTS), CLASS_SLOTS)
     getfield(class, :slots)[idx]
 end
+function get_indirect_slots(class::Instance)
+    idx = findfirst(==(CLASS_CPL), CLASS_SLOTS)
+    cpl = getfield(class, :slots)[idx]
 
-get_all_slots(instance::Instance) = println("Not Implemented Exception")
+    indirect_slots = []
+    for class_cpl in cpl
+        if class_cpl == class
+            continue
+        end
 
-get_field_index(instance::Instance, slot_name::Symbol) =
-    findfirst(==(slot_name), get_direct_slots(getfield(instance, :class)))
-####################################################################
+        indirect_slots = vcat(indirect_slots, get_direct_slots(class_cpl))
+    end
+
+    indirect_slots
+end
+
+function get_all_slots(class::Instance)
+    vcat(get_direct_slots(class), get_indirect_slots(class))
+end
+
+function get_field_index(instance::Instance, slot_name::Symbol)
+    findfirst(==(slot_name), get_all_slots(getfield(instance, :class)))
+end
 
 function class_of(instance::Instance)
     getfield(instance, :class)
@@ -57,16 +84,48 @@ function Base.setproperty!(instance::Instance, slot_name::Symbol, value)
 end
 
 ####################################################################
+
+function compute_cpl(class::Instance)
+    cpl = []
+    queue = [class]
+
+    while !isempty(queue)
+        value = popfirst!(queue)
+        if (value ∉ cpl)
+            push!(cpl, value)
+            for superclass in value.direct_superclasses
+                push!(queue, superclass)
+            end
+        end
+    end
+
+    cpl
+
+end
+
+####################################################################
 #                             METHODS                              #
 ####################################################################
 allocate_instance(class::Instance) = Instance(class)
 
 function initialize(instance::Instance, initargs)
-    dump(getfield(instance, :class))
+
     for slot_name in get_direct_slots(getfield(instance, :class))
         value = get(initargs, slot_name, missing)
         push!(getfield(instance, :slots), value)
     end
+
+    if getfield(instance, :class) == Class
+        cpl = compute_cpl(instance)
+        setproperty!(instance, CLASS_CPL, cpl)
+    end
+
+
+    for slot_name in get_indirect_slots(getfield(instance, :class))
+        value = get(initargs, slot_name, missing)
+        push!(getfield(instance, :slots), value)
+    end
+
 end
 
 new(class; initargs...) =
@@ -86,22 +145,22 @@ GenericFunction = new(Class, direct_superclasses=[Object], direct_slots=[:name, 
 MultiMethod = new(Class, direct_superclasses=[Object], direct_slots=[:specializers, :procedure, :generic_function])
 
 function call_effective_method(f, args)
-    
-    
+
+
 end
 
 (f::Instance)(args...) = call_effective_method(f, args...)
 
 
-func = new(GenericFunction, name=:func, args=[:a,:b], methods=[])
+func = new(GenericFunction, name=:func, args=[:a, :b], methods=[])
 
 
 ####################################################################
 
 
-
-ComplexNumber = new(Class, name=:ComplexNumber, direct_superclasses=[Object], direct_slots=[:real, :img])
-ist = new(ComplexNumber, img=1, real=3)
+Num = new(Class, name=:Number, direct_superclasses=[Object], direct_slots=[:value])
+ComplexNumber = new(Class, name=:ComplexNumber, direct_superclasses=[Num, Object], direct_slots=[:real, :img])
+ist = new(ComplexNumber, img=1, value=27, real=3)
 dump(ist)
 
 class_of(Class) == Class
@@ -110,6 +169,7 @@ getproperty(ist, :img)
 getproperty(ist, :real)
 ist.real
 ist.real += 5
+ist.value
 setproperty!(ist, :real, 5634753645763485863)
 getproperty(ist, :real)
 
